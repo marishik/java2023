1cb33c7ea8a5d7235d9d4764c376ed08f66f598f
Marina <marlow.2256@gmail.com>
2023-02-23 16:14:03 +0500

    Добавлены методы sayHello, sinusTable

---
+++ src/Program.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Program {
+    public static void sayHello()
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите имя: ");
+        String name = in.nextLine();
+        System.out.println("Привет, " + name);
+    }
+
+    public static void sinusTable() {
+        System.out.println("Таблица синусов от 0 до 45 градусов:\n");
+        System.out.println("+------------------------+");
+        System.out.printf("|%-10s | %-10s |\n", "degree", "sinus");
+        System.out.println("+------------------------+");
+
+            for (int k = 0; k < 50; k = k + 5){
+                double angle = Math.toRadians(k);
+                System.out.printf("|%-10s | %-10f |\n", k, Math.sin(angle));
+                System.out.println("+------------------------+");
+            }
+    }
+}
