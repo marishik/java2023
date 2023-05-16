8c1f4cf1f0fa4e6b0d2f1b901b199b0922685dc7
Marina <marlow.2256@gmail.com>
2023-05-16 20:40:02 +0500

    Реализовано движение значений по ячейкам и основная логика игры. Написаны модульные тесты

--- src/Game2048.java
+++ src/Game2048.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 public class Game2048 extends Game{
     private static final int SIDE = 4;
     private int[][] gameField = new int[SIDE][SIDE];
+    private boolean isGameStopped = false;
 
     public void initialize() {
         setScreenSize(SIDE, SIDE);
@@ -30,6 +31,9 @@ public class Game2048 extends Game{
      * Генерирует с вероятностью 90% значение "2" и с вероятностью 10% значение "4" для первых значений в рандомную ячейку с координатами x и y.
      */
     private void createNewNumber(){
+        if(getMaxTileValue() == 2048){
+            win();
+        }
         int x = getRandomNumber(SIDE);
         int y = getRandomNumber(SIDE);
         int chance = getRandomNumber(10);
@@ -57,22 +61,35 @@ public class Game2048 extends Game{
      * Метод устанавливает цвета для всех возможных значений (которые будут реализованы в игре) в клетке и возвращает цвет для необходимого значения
      * @param value - числовое значение в ячейке
      */
-    private Color getColorByValue(int value){
-        HashMap<Integer, Color> colorByValue = new HashMap<>();
-        colorByValue.put(0, Color.LAVENDERBLUSH);
-        colorByValue.put(2, Color.LAVENDER);
-        colorByValue.put(4, Color.THISTLE);
-        colorByValue.put(8, Color.ORCHID);
-        colorByValue.put(16, Color.MEDIUMORCHID);
-        colorByValue.put(32, Color.DARKORCHID);
-        colorByValue.put(64, Color.BLUEVIOLET);
-        colorByValue.put(128, Color.DARKVIOLET);
-        colorByValue.put(264, Color.PURPLE);
-        colorByValue.put(512, Color.DARKMAGENTA);
-        colorByValue.put(1024, Color.MEDIUMVIOLETRED);
-        colorByValue.put(2048, Color.VIOLET);
-
-        return colorByValue.get(value);
+    public static Color getColorByValue(int value){
+        switch (value) {
+            case 0:
+                return Color.WHITE;
+            case 2:
+                return Color.PLUM;
+            case 4:
+                return Color.SLATEBLUE;
+            case 8:
+                return Color.DODGERBLUE;
+            case 16:
+                return Color.DARKTURQUOISE;
+            case 32:
+                return Color.MEDIUMSEAGREEN;
+            case 64:
+                return Color.LIMEGREEN;
+            case 128:
+                return Color.DARKORANGE;
+            case 256:
+                return Color.SALMON;
+            case 512:
+                return Color.ORANGERED;
+            case 1024:
+                return Color.DEEPPINK;
+            case 2048:
+                return Color.MEDIUMVIOLETRED;
+            default:
+                return Color.NONE;
+        }
     }
 
     /***
@@ -91,12 +108,75 @@ public class Game2048 extends Game{
 
     }
 
+    @Override
+    public void onKeyPress(Key key) {
+        if (key == Key.LEFT) {
+            moveLeft();
+        }
+        else if (key == Key.RIGHT) {
+            moveRight();
+        }
+        else if (key == Key.DOWN) {
+            moveDown();
+        }
+        else if (key == Key.UP) {
+            moveUp();
+        }
+        else {
+            return;
+        }
+        drawScene();
+    }
+
+    /***
+     * Cдвигает элементы каждой строки матрицы gameField методом compressRow(int[]),
+     * после соединяет их значения с помощью метода mergeRow(int[]),
+     * а затем снова сдвигаем (так как после соединения могут образоваться пустые промежутки)
+     */
+    private void moveLeft() {
+        boolean isNewNumberNeeded = false;
+        for (int[] row : gameField) {
+            boolean wasCompressed = compressRow(row);
+            boolean wasMerged = mergeRow(row);
+            if (wasMerged) {
+                compressRow(row);
+            }
+            if (wasCompressed || wasMerged) {
+                isNewNumberNeeded = true;
+            }
+        }
+        if (isNewNumberNeeded) {
+            createNewNumber();
+        }
+    }
+
+
+    // moveRight, moveDown, moveUp реализованы так, что мы вращаем gameField до тех пор, пока не придется сдвигать элементы влево,
+    // а метод для сдвигания влево уже есть, так что пользуемся им. В конце возвращем gameField в исходное положение
+    private void moveRight() {
+        rotateXtimes(2);
+        moveLeft();
+        rotateXtimes(2);
+    }
+
+    private void moveDown() {
+        rotateClockwise();
+        moveLeft();
+        rotateXtimes(3);
+    }
+
+    private void moveUp() {
+        rotateXtimes(3);
+        moveLeft();
+        rotateClockwise();
+    }
+
     /***
      * Метод "перебрасывает" все ненулевые элементы влево, а нули вправо
      * @param row
      * @return flag - сделана ли перестановка влево
      */
-    private boolean compressRow(int[] row){
+    public static boolean compressRow(int[] row){
         boolean flag = false;
         for(int i = 0; i < row.length; i++){
             for (int j = 0; j < row.length - i - 1; j++) {
@@ -111,5 +191,64 @@ public class Game2048 extends Game{
         }
 
         return flag;
+    }
+
+    /***
+     * Проверяет, были ли рядом в строке одинаковые значения, которые следует соединить
+     * @return
+     */
+    public static boolean mergeRow(int[] row) {
+        boolean flag = false;
+        for (int i = 0; i < row.length - 1; i++) {
+            if (row[i] == row[i + 1] && row[i] != 0) {
+                row[i] = row[i] * 2;
+                row[i + 1] = 0;
+                flag = true;
+            }
+        }
+
+        return flag;
+    }
+
+
+    /***
+     * Вращает матрицу gameField по часовой стрелке на 90 градусов
+     */
+    private void rotateClockwise(){
+        int[][] newGameField = new int[SIDE][SIDE];
+        for(int y = 0; y < gameField.length; y++){
+            for (int i = 0; i < SIDE; i++) {
+                for (int j = 0; j < SIDE; j++) {
+                    newGameField[j][SIDE - 1 - i] = gameField[i][j];
+                }
+            }
+        }
+        gameField = newGameField;
+    }
+
+    /***
+     * Считает максимальное значение, которое сейчас есть в матрице. Проверка на победу :)
+     */
+    public int getMaxTileValue(){
+        int result = 0;
+
+        for (int[] i : gameField) {
+            for (int j : i)
+                result = Math.max(result, j);
+        }
+
+        return result;
+    }
+
+    private void win(){
+        isGameStopped = true;
+        showMessageDialog(Color.SILVER, "You win!", Color.TOMATO, 12);
+    }
+
+    private void rotateXtimes(int howManyTimes){
+        while(howManyTimes > 0) {
+            rotateClockwise();
+            howManyTimes--;
+        }
     }
 }
---
+++ src/TestGame2048.java
@@ -0,0 +1,61 @@
+import com.javarush.engine.cell.Color;
+import com.javarush.engine.cell.Game;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestGame2048 extends Game {
+
+    @Test
+    void testGetColorByValue10(){
+        //int[][] arr = {{10,20,30}, {5,7,8,99,14}, {}, {91}};
+        Color expected = Color.NONE;
+        Color actual = Game2048.getColorByValue(10);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testGetColorByValue2048(){
+        //int[][] arr = {{10,20,30}, {5,7,8,99,14}, {}, {91}};
+        Color expected = Color.MEDIUMVIOLETRED;
+        Color actual = Game2048.getColorByValue(2048);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testCompressCouldBeDone(){
+        int[] arr = {2, 0, 0, 2};
+        boolean expected = true;
+        boolean actual = Game2048.compressRow(arr);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testCompressRowCouldNotBeDone(){
+        int[] arr = {2, 0, 0, 0};
+        boolean expected = false;
+        boolean actual = Game2048.compressRow(arr);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testMergeRowTrue(){
+        int[] arr = {2, 2, 0, 0};
+        boolean expected = true;
+        boolean actual = Game2048.mergeRow(arr);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testMergeRowFalse(){
+        int[] arr = {2, 0, 2, 0};
+        boolean expected = false;
+        boolean actual = Game2048.mergeRow(arr);
+
+        assertEquals(expected, actual);
+    }
+}
